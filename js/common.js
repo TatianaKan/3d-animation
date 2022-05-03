9ac50099bdb8de32db5a995d477d3839d0eff1f7
TatianaKan <klanukt@gmail.com>
2022-05-03 16:54:10 +0300

    animation, slider

--- js/common.js
+++ js/common.js
@@ -1,7 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
 	// $('body').hide()
+	const showSlider = new Swiper('.showcase-carusel', {
+		loop: true,
+		slidesPerView: 3,
+		speed: 1800,
+		centeredSlides: true,
+	})
 
-
-	
+	document.querySelector('video').playbackRate = 2;
 })
