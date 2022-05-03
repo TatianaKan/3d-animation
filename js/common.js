1c5b4fc5567dafaea8735f0bc0ea7c3ec0cc08da
TatianaKan <klanukt@gmail.com>
2022-05-03 17:10:45 +0300

    navigation

--- js/common.js
+++ js/common.js
@@ -6,6 +6,10 @@ document.addEventListener('DOMContentLoaded', function () {
 		slidesPerView: 3,
 		speed: 1800,
 		centeredSlides: true,
+		navigation: {
+			nextEl: '.showcase-navigation__next',
+			prevEl: '.showcase-navigation__prev',
+		}
 	})
 
 	document.querySelector('video').playbackRate = 2;
